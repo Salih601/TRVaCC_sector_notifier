51f3e5406700e6f1b479298a1172019e69b835cf
Salih601 <salihrifat2009@gmail.com>
2026-01-23 01:11:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
+import requests
+import os
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+def send_telegram(message):
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+    response = requests.post(url, json=payload)
+    print(response.text)
+
 if __name__ == "__main__":
     send_telegram("✅ GitHub Actions test mesajı: bot çalışıyor")
